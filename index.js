c3a1d6aec9c4c78a5f8babb799bd528e961d96c2
Wesam Manassra <wesam.manassra@gmail.com>
2020-11-11 22:50:41 -0800

    update file

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ import fs from "fs";
     "https://gbfs.baywheels.com/gbfs/en/free_bike_status.json"
   );
   const content = await response.json();
-  fs.writeFileSync("./sf.json", JSON.stringify(content.data.bikes));
+  fs.writeFileSync(
+    "./sf.json",
+    JSON.stringify(
+      content.data.bikes.map((bike) => ({
+        ...bike,
+        lat: bike.lat + Math.random() * 0.00001,
+        lon: bike.lon + Math.random() * 0.00001,
+      }))
+    )
+  );
 })();
